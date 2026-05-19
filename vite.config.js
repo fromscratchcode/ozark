5f08669c6adf27aa5a2c4e9da5a38566c9bbe762
Tyler Green <tyler@fromscratchcode.com>
2026-05-19 18:30:40 -0400

    Package for standalone use

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: "./src/index.js",
-      formats: ["es"],
-      fileName: () => "index.js",
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-    },
-  },
-});
+  build:
+    mode === "lib"
+      ? {
+          lib: {
+            entry: "./src/index.js",
+            formats: ["es"],
+            fileName: () => "index.js",
+          },
+          rollupOptions: {
+            external: ["react", "react-dom"],
+          },
+        }
+      : undefined,
+}));
